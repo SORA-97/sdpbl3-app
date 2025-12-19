6644e1965c3eab3e002db7bfcfeaff8a168d8ac0
SORA-97 <sora.versatile@gmail.com>
2025-12-19 11:15:07 +0900

    add html template

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "SD PBL APP"
+def home():
+    return render_template("index.html")
+
+@app.route("/favicon.ico")
+def favicon():
+    return "", 204
 
 if __name__ == "__main__":
     app.run()
